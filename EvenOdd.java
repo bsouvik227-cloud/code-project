4a9c01e5da32c11545c86b821bde5c57999096f3
Souvik Biswas.JR10 <bsouvik227@gmail.com>
2026-03-29 10:32:14 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");
+        }
+
+        sc.close();
+    }
+}
